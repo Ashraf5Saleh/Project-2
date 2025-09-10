c77e8b4222b711e019c67db7bbcb45d32bcb543b
Ashraf5Saleh <ashrafsaleh8602@gmail.com>
2025-09-10 00:52:44 -0700

    feat: file creation

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig, loadConfigFromFile } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 4000
+  }
+})
